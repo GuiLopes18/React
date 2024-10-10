fec0f8d405c6583ab6c7844d9223e3e60191f06b
Guilherme Lopes <107151415+GuiLopes18@users.noreply.github.com>
2024-10-10 18:35:17 +0000

    wooooow

---
+++ aula13/loginpage/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; // Se você ainda usar o App.css
+import Login from './Login'; // Importe o componente de Login
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ aula13/loginpage/src/Login.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Login.css'; // Importa o arquivo CSS
+
+function Login() {
+  return (
+    <div className="login-page">
+      <header className="header">
+        <h1>Monitoria React</h1>
+        <a href="/contact">Contato</a>
+      </header>
+      <div className="login-container">
+        <h2>Login</h2>
+        <form>
+          <input type="text" placeholder="Usuário" />
+          <input type="password" placeholder="Senha" />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
