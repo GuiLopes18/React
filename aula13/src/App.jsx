bdbb0b5f6b67c566f5c54eb8e3ea841411f1398e
Guilherme Lopes <107151415+GuiLopes18@users.noreply.github.com>
2024-10-10 13:38:48 -0300

    Primeira aula da biblioteca React

---
+++ aula13/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Login from './Login';
+
+function App() {
+  return (
+    <div class="App">
+      <Header />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ aula13/src/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const Header = () => {
+  return (
+    <header class="header">
+      <h1 classe="header-title">Monitoria React</h1>
+      <nav>
+        <ul>
+          <li class="header-link">Contato</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
